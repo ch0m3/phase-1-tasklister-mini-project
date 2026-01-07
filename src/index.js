f817880e78da4c1e1396f8a64d91417839d07d51
Chome <chomeallan@gmail.com>
2026-01-07 19:39:52 +0300

    Listing stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form")
+  //const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = document.querySelector('#new-task-description')
+    const taskList = document.querySelector('#tasks')
+
+    const li = document.createElement('li')
+    li.textContent = input.value;
+    taskList.appendChild(li);
+  })
 });
